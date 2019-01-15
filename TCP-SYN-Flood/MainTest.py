b726384b7a7610de91a345f2586b6b590e92bddc
Joaquin Cruz <Joacocruz6@gmail.com>
2019-01-15 10:55:10 -0300

    modifique unos test de TCP-SYN-Flood

--- TCP-SYN-Flood/MainTest.py
+++ TCP-SYN-Flood/MainTest.py
@@ -36,5 +36,5 @@ if __name__=="__main__":
         else:
             for i in range(1,len(args)):
                 MainTest().run(args[i])
-    except:
+    except IndexError:
         raise Exception("Se utiliza de la forma:\nTest a realizar (all o nada para todos)")
